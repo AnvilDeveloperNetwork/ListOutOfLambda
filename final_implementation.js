348a76c8c61e7fb755cbf43ad04642759a584d70
marsha88 <marsha88@purdue.edu>
2018-11-14 12:32:15 -0500

    corrected map and filter in final impl

--- final_implementation.js
+++ final_implementation.js
@@ -15,11 +15,25 @@ const prepend = (head, tail) => (selector) => selector(head, tail, falsey)
 const head = list => list((head, tail) => head)
 const tail = list => list((head, tail) => tail)
 
+/***** Create List Helper ******/
+
+const createList = (...args) => args.reverse().reduce((acc, el) => prepend(el, acc), empty)
+
+/*******************************/
 
 /***** More List Helper Functions ******/
 
-const length = list => ifelse(isEmpty(list), _ => 0, _ => 1 + length(tail(list)))
-const map = (f, list) => ifelse(isEmpty(list), _ => empty, prepend(f(head(list)), map(f, tail(list))))
-const filter = (f, list) => ifelse(isEmpty(list), _ => empty, )
+const length = list => ifelse(isEmpty(list),
+                                _ => 0,
+                                _ => 1 + length(tail(list)))
 
+const map = (f, list) => ifelse(isEmpty(list),
+                                  _ => empty,
+                                  _ => prepend(f(head(list)), map(f, tail(list))))
+
+const filter = (f, list) => ifelse(isEmpty(list),
+                                    _ => empty,
+                                    _ => ifelse(f(head(list)),
+                                              _ => prepend(head(list), filter(f, tail(list))),
+                                              _ => filter(f, tail(list))))
 /**************************************/
